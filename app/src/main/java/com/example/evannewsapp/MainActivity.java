668fd37fcca97b806f93ce0183eabed3001276f0
Brian Donnoe <dunow33@hotmail.com>
2023-05-21 21:42:20 -0400

    at about 49 minutes

--- app/src/main/java/com/example/evannewsapp/MainActivity.java
+++ app/src/main/java/com/example/evannewsapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.evannewsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import com.example.evannewsapp.models.NewsApiResponse;
@@ -13,11 +15,16 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     CustomAdapter adapter;
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Fetching news articles....");
+        dialog.show();
 
         RequestManager manager = new RequestManager(this);
         manager.getNewsHeadlines(listener, "general", null);
@@ -27,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onFetchData(List<NewsHeadlines> list, String message) {
             showNews(list);
+            dialog.dismiss();
         }
 
         @Override
@@ -34,4 +42,12 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+    private void showNews(List<NewsHeadlines> list) {
+        recyclerView = findViewById(R.id.recycler_main);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        adapter = new CustomAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+    }
 }
---
+++ app/src/main/java/com/example/evannewsapp/SelectListener.java
@@ -0,0 +1,4 @@
+package com.example.evannewsapp;
+
+public interface SelectListener {
+}
